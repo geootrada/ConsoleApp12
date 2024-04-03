15c5cdd5b608b3065ec672cbcfaeb1e0ac8896aa
geootrada <geootrada@yandex.ru>
2024-04-03 09:57:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            string stopWord = "exit";
+
+            Console.Write(" Здравствуйте, это программа-напоминатель!\n " +
+               "Введите сообщение, которое вам нужно сегодня напомнить: ");
+            message = Console.ReadLine();            
+
+            while (message !=stopWord)
+            {
+                Console.Write($"Напоминаем ваше сообщение - {message}\n " +
+                "Для завершения программы наберите EXIT или введите новое сообщение: ");
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
